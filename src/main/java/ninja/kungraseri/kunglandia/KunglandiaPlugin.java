4175352ac1f1fad8e3e1a394dc78c7c15e0f9d36
Joshua Homer <jhomer@clouddev.epiqcorp.com>
2018-10-03 15:24:27 -0700

    build works with maven.

---
+++ src/main/java/ninja/kungraseri/kunglandia/KunglandiaPlugin.java
@@ -0,0 +1,16 @@
+package ninja.kungraseri.kunglandia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KunglandiaPlugin extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/test/java/ninja/kungraseri/kunglandia/KunglandiaPluginTest.java
@@ -0,0 +1,20 @@
+package ninja.kungraseri.kunglandia;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for KunglandiaPlugin.
+ */
+public class KunglandiaPluginTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
